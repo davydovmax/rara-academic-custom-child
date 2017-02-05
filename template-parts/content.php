3d8dd0d2d1a58a2326055bb4814d54cdfe2e0adf
Maksym Davydov <maksym.davydov@booking.com>
2017-02-05 17:13:13 +0100

    Don't show posted-on for specific categories

--- template-parts/content.php
+++ template-parts/content.php
@@ -19,7 +19,7 @@
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		}
 
-		if ( 'post' === get_post_type() ) : ?>
+		if ( 'post' === get_post_type() and !hide_post_the_posted_on_text() ) : ?>
 			<div class="entry-meta">
 				<?php rara_academic_posted_on(); ?>
 			</div><!-- .entry-meta -->
