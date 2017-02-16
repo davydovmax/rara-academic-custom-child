e41bd1be635d26765ab858d8c5085d345928eed5
Maksym Davydov <maksym.davydov@booking.com>
2017-02-16 23:52:55 +0100

    Added multi-select control so we can select multiple Categories to behave like pages

--- functions.php
+++ functions.php
@@ -39,4 +39,72 @@ if ( !function_exists( 'hide_post_the_posted_on_text' ) ):
     }
 endif;
 
+/**
+ * Multiple select customize control class.
+ */
+class Customize_Control_Multiple_Select extends WP_Customize_Control {
+
+    /**
+     * The type of customize control being rendered.
+     */
+    public $type = 'multiple-select';
+
+    /**
+     * Displays the multiple select on the customize screen.
+     */
+    public function render_content() {
+
+    if ( empty( $this->choices ) )
+        return;
+    ?>
+        <label>
+            <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+            <select <?php $this->link(); ?> multiple="multiple" style="height: 100%;">
+                <?php
+                    foreach ( $this->choices as $value => $label ) {
+                        $selected = ( in_array( $value, $this->value() ) ) ? selected( 1, 1, false ) : '';
+                        echo '<option value="' . esc_attr( $value ) . '"' . $selected . '>' . $label . '</option>';
+                    }
+                ?>
+            </select>
+        </label>
+    <?php }
+}
+
+if ( !function_exists( 'art_people_customize_register' ) ):
+    function art_people_customize_register( $wp_customize )
+    {
+        $wp_customize->add_section(
+            'rara_academic_art_people_settings',
+            array(
+                'title' => __( 'ArtPeople Settings', 'rara-academic' ),
+                'description' => __( 'Custom Stuff', 'rara-academic' ),
+                'priority' => 200,
+                'capability' => 'edit_theme_options',
+            )
+        );
+
+        $wp_customize->add_setting( 'art_people_post_like_pages' );
+
+        $wp_customize->add_control(
+            new Customize_Control_Multiple_Select(
+                $wp_customize,
+                'multiple_select_setting',
+                array(
+                    'settings' => 'art_people_post_like_pages',
+                    'label'    => 'Page-like categories',
+                    'section'  => 'rara_academic_art_people_settings',
+                    'type'     => 'multiple-select',
+                    'choices'  => array(
+                        'google' => 'Google',
+                        'bing' => 'Bing',
+                        'duck' => 'DuckDuckGo'
+                    )
+                )
+            )
+        );
+    }
+endif;
+add_action( 'customize_register', 'art_people_customize_register' );
+
 // END ENQUEUE PARENT ACTION
